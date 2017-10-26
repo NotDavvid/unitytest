2ccdc543297fbc775bc3edb09ee2a033a13d2a6b
NotDavvid <notdavvid@gmail.com>
2017-10-26 16:41:18 -0500

    do people even read these?

---
+++ Assets/hitcheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitcheck : MonoBehaviour {
+	public Transform note;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (note.position.z < transform.position.z + 1 && note.position.z > transform.position.z - 1) {
+			Debug.Log ("hit");
+		} else if (note.position.z < transform.position.z - 1 && note.position.z > transform.position.z - 2) {
+			Debug.Log ("miss");
+		}
+	}
+
+	void OnTriggerStay(){
+		Debug.Log ("inside");
+	}
+}
--- Assets/notestofire.cs
+++ Assets/notestofire.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class notestofire : MonoBehaviour {
 	//public Transform tile;
 	public Transform prefab;
+	public Transform target;
+
+	void Start(){
+		transform.rotation =  Quaternion.LookRotation(target.position - transform.position);
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey("q")){
 			Transform gameObject = Instantiate(prefab, transform.position, transform.rotation);
 			Rigidbody bullet = gameObject.GetComponent<Rigidbody>();
-			bullet.velocity = transform.TransformDirection(new Vector3(0,0,-1) * 10);
+			bullet.velocity = transform.TransformDirection(Vector3.forward * 10);
+			//bullet.isKinematic = true;
 		}
 	}
 }
