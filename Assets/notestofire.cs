02f4174e29813827c981974a72ebc587585a6865
NotDavvid <notdavvid@gmail.com>
2017-10-26 15:51:26 -0500

    more changes, added a new scene for the other gamemode

---
+++ Assets/notestofire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notestofire : MonoBehaviour {
+	//public Transform tile;
+	public Transform prefab;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("q")){
+			Transform gameObject = Instantiate(prefab, transform.position, transform.rotation);
+			Rigidbody bullet = gameObject.GetComponent<Rigidbody>();
+			bullet.velocity = transform.TransformDirection(new Vector3(0,0,-1) * 10);
+		}
+	}
+}
